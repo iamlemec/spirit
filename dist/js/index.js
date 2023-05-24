3a63704c0444b3c4059b3eb1e484a8fcef367140
Douglas Hanley <thesecretaryofwar@gmail.com>
2023-05-24 11:51:26 -0400

    ship dist

---
+++ dist/js/index.js
@@ -0,0 +1,89 @@
+import { enableResize, SpiritEditor, setCookie, getCookie } from './spirit.js';
+
+// spirit editor
+
+
+class Connection extends EventTarget {
+    constructor(url) {
+        super();
+
+        // initialize websocket
+        this.ws = new WebSocket(url);
+
+        // track connectivity
+        this.ws.addEventListener('open', evt => {
+            console.log('connected');
+            this.dispatchEvent(new Event('open'));
+        });
+
+        // connect real events
+        this.ws.addEventListener('message', evt => {
+            console.log(`received: ${evt.data}`);
+            let {cmd, doc, data} = JSON.parse(evt.data);
+            if (cmd == 'load') {
+                this.dispatchEvent(
+                    new CustomEvent('load', {detail: data})
+                );
+            }
+        });
+    }
+
+    loadDocument(doc) {
+        this.ws.send(JSON.stringify({
+            cmd: 'load', doc
+        }));
+    }
+
+    sendUpdates(doc, upd) {
+        if (this.ws.readyState) {
+            this.ws.send(JSON.stringify({
+                cmd: 'update', doc, data: upd.changes.toJSON()
+            }));
+        }
+    }
+}
+
+function initSpirit(doc) {
+    console.log(`initSpirit: ${doc}`);
+
+    // global elements
+    let left = document.querySelector('#left');
+    let right = document.querySelector('#right');
+    let mid = document.querySelector('#mid');
+
+    // resize panels
+    enableResize(left, right, mid);
+    let editor = new SpiritEditor(left, right);
+
+    // server or no-server mode
+    if (doc) {
+        // connect with server
+        const port = 8000;
+        let connect = new Connection(`ws://localhost:${port}`);
+
+        // connect editor events
+        editor.addEventListener('update', evt => {
+            connect.sendUpdates(doc, evt.detail);
+        });
+
+        // connect server events
+        connect.addEventListener('load', evt => {
+            editor.loadDocument(evt.detail);
+        });
+
+        connect.addEventListener('open', evt => {
+            connect.loadDocument(doc);
+        });
+    } else {
+        // connect cookie storage
+        editor.addEventListener('update', evt => {
+            setCookie('spirit', evt.detail.text);
+        });
+
+        // no server mode
+        let text = getCookie('spirit');
+        editor.loadDocument(text);
+    }
+}
+
+export { initSpirit };
---
+++ dist/js/spirit.js
@@ -0,0 +1,134 @@
+import { EditorView, lineNumbers, keymap } from '../node_modules/@codemirror/view/dist/index.js';
+import { EditorState, Compartment } from '../node_modules/@codemirror/state/dist/index.js';
+import { indentWithTab, defaultKeymap, historyKeymap } from '../node_modules/@codemirror/commands/dist/index.js';
+import { bracketMatching } from '../node_modules/@codemirror/language/dist/index.js';
+import { javascript } from '../node_modules/@codemirror/lang-javascript/dist/index.js';
+import { markdown } from '../node_modules/@codemirror/lang-markdown/dist/index.js';
+import { minimalSetup } from '../node_modules/codemirror/dist/index.js';
+import { parseDocument } from './markum.js';
+
+const readOnly = new Compartment();
+
+function readWriteEditor(parent, update) {
+    return new EditorView({
+        state: EditorState.create({
+            doc: '',
+            extensions: [
+                minimalSetup,
+                lineNumbers(),
+                bracketMatching(),
+                markdown({defaultCodeLanguage: javascript()}),
+                keymap.of([
+                    indentWithTab,
+                    ...defaultKeymap,
+                    ...historyKeymap,
+                ]),
+                EditorView.lineWrapping,
+                EditorView.updateListener.of(update),
+                readOnly.of(EditorState.readOnly.of(true))
+            ],
+        }),
+        parent: parent,
+    });
+}
+
+function getText(state) {
+    return state.doc.toString();
+}
+
+function setText(editor, text) {
+    let len = editor.state.doc.length;
+    let upd = editor.state.update({
+        changes: {from: 0, to: len, insert: text}
+    });
+    editor.dispatch(upd);
+}
+
+class SpiritEditor extends EventTarget {
+    constructor(code, disp) {
+        super();
+        this.code = code;
+        this.disp = disp;
+        this.emit = false;
+        this.edit = readWriteEditor(code, upd => {
+            if (this.emit && upd.docChanged) {
+                this.applyUpdate(upd);
+            }
+        });
+    }
+
+    loadDocument(src) {
+        if (src) {
+            this.emit = false;
+            this.setCode(src);
+            this.setDisp(src);
+        }
+        this.emit = true;
+        this.setReadOnly(false);
+    }
+
+    applyUpdate(upd) {
+        let text = getText(upd.state);
+        let detail = {text, changes: upd.changes};
+        this.setDisp(text);
+        this.dispatchEvent(
+            new CustomEvent('update', {detail})
+        );
+    }
+
+    setReadOnly(flag) {
+        this.edit.dispatch({
+            effects: readOnly.reconfigure(EditorState.readOnly.of(flag))
+        });
+    }
+
+    setCode(src) {
+        setText(this.edit, src);
+    }
+
+    setDisp(src) {
+        let tree = parseDocument(src);
+        let html = tree.html();
+        this.disp.innerHTML = html;
+    }
+}
+
+// cookie tools
+function getCookie(key) {
+    let cookies = document.cookie.split(';').map(x => x.trim().split('='));
+    let cell = cookies.filter(([k, v]) => k == key).shift();
+    if (cell == null) {
+        return null;
+    } else {
+        let [_, val] = cell;
+        return decodeURIComponent(val);
+    }
+}
+
+function setCookie(key, val) {
+    let enc = encodeURIComponent(val);
+    document.cookie = `${key}=${enc}; SameSite=Lax`;
+}
+
+// resize panels
+function enableResize(left, right, mid) {
+    let base = left.getBoundingClientRect().left;
+    function resizePane(e) {
+        let vw = window.innerWidth;
+        let x = e.clientX;
+        let lw = Math.max(200, x - 2 - base);
+        let rw = Math.max(200, vw - x - 2);
+        left.style.width = `${lw}px`;
+        right.style.width = `${rw}px`;
+    }
+    
+    mid.addEventListener('mousedown', evt => {
+        document.addEventListener('mousemove', resizePane, false);
+    }, false);
+    
+    document.addEventListener('mouseup', evt => {
+        document.removeEventListener('mousemove', resizePane, false);
+    }, false);
+}
+
+export { SpiritEditor, enableResize, getCookie, setCookie };
